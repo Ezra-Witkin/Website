8046a3c6bbef049278b2b2ed3398a2edcde767d5
Ezra-Witkin <eaw5156@g.rit.edu>
2024-12-26 15:09:12 -0800

    Update menu.php
    delete

--- menu.php
+++ menu.php
@@ -1,12 +1,1 @@
-<!-- menu.php -->
-<img src="Images/catLineDrawing.png" alt="abstract cat line drawing" width="84" height="66">
-<h1><a href="home.html">All About Cats</a></h1>
-<nav>
-    <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="aHealingPurr.html">A healing purr</a></li>
-        <li><a href="sphynxCats.html">Sphynx Cats</a></li>
-        <li><a href="catColds.html">Cat Colds</a></li>
-        <li><a href="sleepyCats.html">Sleepy Cats</a></li>
-    </ul>
-</nav>
+
